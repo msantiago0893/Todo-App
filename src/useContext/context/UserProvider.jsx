dcd64064012c4215947d62d608a691c479e97c2d
Marco A. Santiago Feria <94033822+msantiago0893@users.noreply.github.com>
2023-10-07 16:04:55 -0600

    Update UserProvider.jsx

--- src/useContext/context/UserProvider.jsx
+++ src/useContext/context/UserProvider.jsx
@@ -12,7 +12,7 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState();
 
   return (
-    <UserContext.Provider value={{ user, setUser }}> {/**  */}
+    <UserContext.Provider value={{ user, setUser }}>
       { children }
     </UserContext.Provider>
   )
